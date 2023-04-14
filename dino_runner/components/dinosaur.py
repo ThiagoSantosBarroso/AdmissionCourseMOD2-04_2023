1d909a1a3eb8ab85435d970dd5c9a38baa9bce83
yKotonet <130694906+ThiagoSantosBarroso@users.noreply.github.com>
2023-04-14 01:05:37 -0300

    Duck

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,11 +1,12 @@
 import pygame
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 class Dinosaur:
 
     X_POS = 80
     Y_POS = 310
     JUMP_VEL = 8.5
+    DUCK_VEL = 8.5
     
     def __init__(self):
         self.image = RUNNING[0]
@@ -16,21 +17,33 @@ class Dinosaur:
         self.dino_run = True
         self.dino_jump = False
         self.jump_vel = self.JUMP_VEL
+        self.dino_duck = False
+        self.duck_vel = self.DUCK_VEL
 
     def update(self, user_input):
         if self.dino_run:
             self.run()
-        elif self.dino_jump:
+        if self.dino_jump:
             self.jump()
+        if self.dino_duck:
+            self.duck()
 
         if self.step_index >= 10:
             self.step_index = 0
 
-        if user_input[pygame.K_UP] and not self.dino_jump:
+        if user_input[pygame.K_UP] and not self.dino_jump and not self.dino_duck:
             self.dino_jump = True
+            self.dino_duck = False
             self.dino_run = False
-        elif not self.dino_jump:
+
+        elif user_input[pygame.K_DOWN] and not self.dino_jump and not self.dino_duck:
             self.dino_jump = False
+            self.dino_duck = True
+            self.dino_run = False
+
+        elif not self.dino_jump and not self.dino_duck:
+            self.dino_jump = False
+            self.dino_duck = False
             self.dino_run = True
 
     def run(self):
@@ -51,6 +64,19 @@ class Dinosaur:
             self.dino_rect.y = self.Y_POS
             self.dino_jump = False
             self.jump_vel = self.JUMP_VEL
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        if self.dino_duck:
+            self.dino_rect.y = 340
+            self.dino_rect.x = self.X_POS
+            self.duck_vel -= 10
+
+        if self.duck_vel < -self.DUCK_VEL:
+            self.dino_duck = False
+            self.duck_vel = self.DUCK_VEL
+        self.step_index += 1
 
     def draw(self, screen: pygame.Surface):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
